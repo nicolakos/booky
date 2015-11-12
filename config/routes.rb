01d8b228f400f6470adf67b0d70cbf2ae838ca59
Nikos <nikosm@eproductions.gr>
2015-11-12 18:03:31 +0200

    Step 2.2

--- config/routes.rb
+++ config/routes.rb
@@ -59,6 +59,7 @@ Rails.application.routes.draw do
 
   get 'about' => 'about#index'
 
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
