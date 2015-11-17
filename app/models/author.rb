4d2a59ed60f82d7118457702c075aa09bea843b1
Nikos <nikosm@eproductions.gr>
2015-11-17 18:04:15 +0200

    Step 2.6

--- app/models/author.rb
+++ app/models/author.rb
@@ -2,6 +2,5 @@ class Author < ActiveRecord::Base
   has_many :books
 
   validates :first_name, presence: true
-  validates :last_name, presence: true,
-              # length: {minimum:3}
+  validates :last_name, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151116144821) do
+ActiveRecord::Schema.define(version: 20151117100728) do
 
   create_table "authors", force: :cascade do |t|
     t.string   "first_name", limit: 255
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20151116144821) do
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
     t.string   "price",        limit: 255
+    t.string   "coverpath",    limit: 255
   end
 
   create_table "categories", force: :cascade do |t|
