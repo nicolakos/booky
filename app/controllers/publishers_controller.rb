30cf82da5a8730060d063d69cf0c78f8379db02c
nicolakos <nikmoustakis@hotmail.gr>
2015-11-25 17:02:17 +0200

    Step 2.7.1

--- app/controllers/publishers_controller.rb
+++ app/controllers/publishers_controller.rb
@@ -15,15 +15,31 @@ class PublishersController < ApplicationController
   end
 
   def update
+    @publisher = Publisher.find(params[:id])
+
+    @publisher.update(publisher_params)
+
+    flash[:notice] = 'publisher Added'
+
+    redirect_to publishers_path
   end
 
   def edit
+    @publisher = Publisher.find(params[:id])
   end
 
   def destroy
+    @publisher = Publisher.find(params[:id])
+
+    @publisher.destroy
+
+    flash[:notice] = 'publisher Removed'
+
+    redirect_to publishers_path
   end
 
   def index
+    @publishers = Publisher.all
   end
 
   def show
